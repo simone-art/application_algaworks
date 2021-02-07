441d364b337e36e08989a9f182d3ce4142e70291
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-07 11:11:48 -0300

    "Colocada a anotation @GetMapping("/index")"

--- src/main/java/com/algaworks/algaworks/controller/ContatoController.java
+++ src/main/java/com/algaworks/algaworks/controller/ContatoController.java
@@ -1,11 +1,13 @@
 package com.algaworks.algaworks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContatoController {
 
     //Criado método para invocar que no browser aparezca a página index
+    @GetMapping("/index")
     public String index(){
         return "index";
     }
