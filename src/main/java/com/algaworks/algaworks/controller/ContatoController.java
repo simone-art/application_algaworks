290e0d86c1cd0085e0b4c97408570e8b2915391b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-07 12:19:41 -0300

    "Criada ArrayList e método estático para adicionar alguns contatos"

--- src/main/java/com/algaworks/algaworks/controller/ContatoController.java
+++ src/main/java/com/algaworks/algaworks/controller/ContatoController.java
@@ -1,13 +1,29 @@
 package com.algaworks.algaworks.controller;
 
+import com.algaworks.algaworks.Contato;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class ContatoController {
 
+    private static final ArrayList<Contato>  LISTA_CONTATOS = new ArrayList<>();
+
+    //Método estático para adicionar alguns contatos
+    static{
+        Contato contato = new Contato();
+        contato.setId("1");
+        contato.setNome("João");
+        contato.setTelefone("11 9999 8888");
+        LISTA_CONTATOS.add(contato);
+    }
+
     //Criado método para invocar que no browser aparezca a página index
-    @GetMapping("/index")
+    //@ @GetMapping Configura que o framework saiba qual enderereço digitado no browser
+    // vai disparar este método e renderizar a p[agina no browser
+    @GetMapping("/")
     public String index(){
         return "index";
     }
