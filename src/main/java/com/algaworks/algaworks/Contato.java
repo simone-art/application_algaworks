477d012c44c01991b4aff5c98fa8c6397556e021
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-08 13:19:06 -0300

    "Informações sobre o padrão Java Bean"

--- src/main/java/com/algaworks/algaworks/Contato.java
+++ src/main/java/com/algaworks/algaworks/Contato.java
@@ -2,6 +2,15 @@ package com.algaworks.algaworks;
 
 public class Contato {
 
+    //Esta classe contato segue o padrão Java Bean: atributos com classe private,
+    // elas não são accessadas externamente. Elas só podems er accesadas por métodos
+    //que pertencem a essa classe. Para ser accesada por outra classe, os atributos devem ser public
+    //Outro padrão são os getters e os setters. set é atribuir valor. Get é obter valor
+    //Resumo do padrão Java Bean: criar propiedades tipo private, e dar acceso a elas através dos
+    //métodos Getters e Setters.
+    //O ThymeLeaf também usa o padrão Java Bean ao invocar o GetNome, por exemplo
+    // que é um método de retorno estático.
+
     //private modificador de accesso do Java para indicar
     // que estes atributos só pertencem a esta classe Contato
     //é só ela poderá permitir o acceso a seus métodos
@@ -22,6 +31,13 @@ public class Contato {
         this.id = id;
         this.nome = nome;
         this.telefone = telefone;
+    }
+
+    //Método criado pars saber se pessoa que está usando o cadastro,
+    // está fazendo um novo ou alterando algum dado.
+    // Se ele for null, o cadastro é novo. Senão, ele é atualizado.
+    public boolean IsNovo(){
+        return id == null;
     }
 
     //Encapsulamento com o Get o Setter
